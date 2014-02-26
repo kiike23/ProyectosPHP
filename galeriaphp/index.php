14e13a42ffe5b85ed9ebdc0d6e16a8930f7af0ef
Enrique <tupsi.love@gmail.com>
2014-02-26 14:09:14 -0600

    - Inicio ahora tiene un menu de navegacion, sin estilo - solo muestra las imagenes existentes en la base de datos

--- galeriaphp/index.php
+++ galeriaphp/index.php
@@ -20,68 +20,42 @@
     <body>
         
         <div class="galeria">
-            <h1>Mis Imagenes</h1>
+            <h1>Mi galería de imágenes</h1>
             <?php
                 require 'config.php';
                 require 'GestorArchivos.php'; 
                 
                 $conexion = new mysqli($servidor, $usuarioBD, $passwordBD, $baseDatos);
                 
-                $consulta = "SELECT archivo, directorio FROM galeriaimagenes ORDER BY id";
+                $consulta = "SELECT archivo, directorio FROM galeriaimagenes ORDER BY archivo"; # ordenar por nombre de archivo
                 $resultado = $conexion->query($consulta);
 
-                // Muestra las imagenes de la galeria.
+            ?>
+        </div>
+        
+        <div class="menu">
+            <nav class="horizontal-menu">
+                <ul>
+                    <li><a href="index.php">Inicio galeria</a></li>
+                    <li><a href="Cargarimagenes.php">Cargar Imágenes</a></li>
+                    <li><a href="Galeria.php">Galeria</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="galeria">
+        <?php
+        // Muestra las imagenes de la galeria.
                 while($filas = $resultado->fetch_array(MYSQLI_ASSOC)) {
                     // Se comprueba que existan las imagenes
                     if (file_exists("imagenes/".$filas["directorio"]."/".$filas["archivo"])){
                         echo'<a href="imagenes/'.$filas['directorio'].'/'.$filas['archivo'].'" rel="lightbox[galeria]" title="'.$filas['archivo'].'"><img src="imagenes/'.$filas['directorio'].'/'.$filas['archivo'].'"/></a>';
                     }                    
                 }
-            ?>
+        ?>
         </div>
-        
-        <div id="subirImagenes">
-                    
-            <!-- Para poder subir archivos con PHP hay que poner enctype="multipart/form-data" para que no se encripte ningun caracter y el archivo no se modifique/estropee -->
-            <form action="#" method="POST" enctype="multipart/form-data">
-                <table id="formularioSubida" border="0">
-                    <thead>
-                        <th>Elige los archivos que quieras subir</th>
-                    </thead>
-                    <tr>
-                        <td>
-                            <div class="inputImagenModificado">
-                                <input class="inputImagenOculto" name="imagen1" type="file">
-                                <div class="inputParaMostrar">
-                                    <input>
-                                    <img src="imagenes/button_select2.gif">
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> 
-                            <input type="button" id="botonAnnadir" onClick="agregarFila('formularioSubida','botonAnnadir')" value="Añadir archivo" style="width:138px;">        
-                            <input type="submit" name="botonSubir" value="Subir"> 
-                        </td>
-                    </tr>
-                </table>
-            </form>
-            
-            <?php
-                // Subir todas las imagenes
-                if(isset($_POST['botonSubir'])){
-                    subirImagenes('prueba',$conexion);
-                }
-            ?>
-            <br />
-        </div>  
-        
         <!-- Muestra el estilo modificado para el input file y cambia la opacidad de las imagenes de la galeria     -->
-        <body onLoad="
-            mostrarInputFileModificado('imagen1');
-            cambiarOpacidadImagenes(0.5);
-        ">
+        <body>
+
 
     </body>
 </html>
